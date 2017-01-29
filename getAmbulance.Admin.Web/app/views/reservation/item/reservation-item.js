9306aebc7773a8bfc797313521d6326e9393f38e
Sari <sari.q34@gmail.com>
2017-01-30 00:22:32 +0200

    fix

--- getAmbulance.Admin.Web/app/views/reservation/item/reservation-item.js
+++ getAmbulance.Admin.Web/app/views/reservation/item/reservation-item.js
@@ -7,7 +7,7 @@
  * Controller of the sbAdminApp
  */
 angular.module('sbAdminApp')
-  .controller('ReservationItemCtrl', function ($scope, ReservationService, Reservations) {
+  .controller('ReservationItemCtrl', function ($scope, ReservationService) {
       $scope.selectedReservationId = ReservationService.getSelectedReservationId();
       ReservationService.getReservationById($scope.selectedReservationId).then(function (res) {
           $scope.reservation = res.data;
