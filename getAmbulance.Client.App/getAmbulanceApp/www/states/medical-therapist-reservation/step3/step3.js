7d28a0b1816a46f56c45662dee0d4048117d0e1c
Sari <sari.q34@gmail.com>
2017-03-25 00:30:42 +0300

    medical ass checkbox groub validation

--- getAmbulance.Client.App/getAmbulanceApp/www/states/medical-therapist-reservation/step3/step3.js
+++ getAmbulance.Client.App/getAmbulanceApp/www/states/medical-therapist-reservation/step3/step3.js
@@ -1,19 +1,38 @@
 
 
-angular.module('starter.controllers').controller('MedicalTherapistStep3Ctrl', function ($scope, $ionicModal, ReservationService, localStorageService, $state) {
+angular.module('starter.controllers').controller('MedicalTherapistStep3Ctrl', function ($scope, $ionicModal, ReservationService, localStorageService, $state, $timeout) {
     $scope.headerInfoText = 'Header_Info_Common_Text';
     var storageFormData = ReservationService.getReservationFormData();
-    if (storageFormData) {
-        $scope.form = storageFormData;
-    } else {
-        $scope.form = {};
+    
+        if (storageFormData) {
+            $scope.form = storageFormData;
+         
+        } else {
+            $scope.form = {};
+        }
+  
+  
+    
+
+    $scope.validateCheckBox = function () {
+        var flag = false;
+        if ($scope.form.Service_Options){
+        angular.forEach($scope.form.Service_Options, function (value, key) {
+            if (value) {
+                flag= true;
+            }
+        })
+        }
+        return flag;
     }
-
-
+    $scope.isCheckBoxGValid = true;
     $scope.continueToStep3 = function () {
+        $scope.isCheckBoxGValid = $scope.validateCheckBox();
+        if ($scope.isCheckBoxGValid) {
         ReservationService.mergeToFormData($scope.form);
         self.fullForm = localStorageService.get('reservationFormData');
         $state.go('app.whitelabel-offers-list');
+        }
     }
 
 
